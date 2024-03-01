9569223fa836eb79907d78a339d8862ed2ea8ef9
mirzakarabulut <mirzakara1234@gmail.com>
2024-03-01 16:40:35 +0300

    Added slider to movie page

--- js/script.js
+++ js/script.js
@@ -74,17 +74,62 @@ async function displayTVDetails() {
   const tvID = await fetchApiData(`tv/${tvDetails}`);
   console.log(tvID);
   displayBackgroundImage('tv', tvID.backdrop_path);
-  console.log("test");
+  const div = document.createElement('div');
+  div.innerHTML = `
+      <div class = "details-top">
+        <div>
+               ${
+            tvID.poster_path
+              ? `<img
+          src="https://image.tmdb.org/t/p/w500${tvID.poster_path}"
+          class="card-img-top"
+          alt="${tvID.name}"
+        />`
+              : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${tvID.name}"
+        />`
+              }
+          <h2>${tvID.name}</h2>
+          <p>
+            <i class="fas fa-star text-primary"></i>
+            ${tvID.vote_average.toFixed(1)}
+          </p>
+          <p class="text-muted">Last Air Date: ${tvID.last_air_date}</p>
+          <p>
+            ${tvID.overview}  
+          </p>
+          <h5>Genres</h5>
+          <ul class="list-group">
+              ${tvID.genres.map((genre) =>`<li>${genre.name}</li>`).join('')}
+          </ul>
+          <a href="#" target="_blank" class="btn">Visit Show Homepage</a>
+        </div>
+      </div>
+      <div class="details-bottom">
+        <h2>Show Info</h2>
+        <ul>
+          <li><span class="text-secondary">Number Of Episodes:</span> ${tvID.number_of_episodes}</li>
+          <li>
+            <span class="text-secondary">Last Episode To Air:</span> ${tvID.last_episode_to_air.name}
+          </li>
+          <li><span class="text-secondary">Status:</span> ${tvID.status}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">${tvID.production_companies.map((company) => `<span>${company.name}</span>`).join(',  ')}</div>
+        </div>
+       </div> 
+  `;
+  document.querySelector('#show-details').appendChild(div);
 };
 
 // Movie Details Page
 async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
   const movID = await fetchApiData(`movie/${movieId}`);
-
   displayBackgroundImage('movie', movID.backdrop_path);
   const div = document.createElement('div');
-  console.log(movID);
     div.innerHTML = `
     <div class="details-top">
                ${
@@ -132,6 +177,52 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(div);
 };
 
+
+// Display Slider Movies
+async function displaySlider()
+{
+  const { results } = await fetchApiData('movie/now_playing');
+  results.forEach((movie) =>
+  {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+         <a href="movie-details.html?id=${movie.id}">
+           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+         </a>
+        <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)}
+        </h4>
+    `;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+    initSwiper();
+  })
+}
+
+function initSwiper()
+{
+   const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
 // Overlay Backgrouns Image
 function displayBackgroundImage(type, backgroundImage)
 {
@@ -203,6 +294,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displaySlider();
       displayPopularMovies();
       break;
     case '/movie-details.html':
